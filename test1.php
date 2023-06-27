df67d93c0ee3d27975873a6451e3c5abb271ed19
arpita-acufore <arpita.s@acufore.com>
2023-06-27 11:01:55 +0530

    content added in the test branch for pull request

--- test1.php
+++ test1.php
@@ -1,3 +1,13 @@
-<?php
-echo "this is the testing for test branch.";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "file changed in test file to check pull request";
+    ?>
+</body>
+</html>
