92f36f9bada3921664e759574fe1de9710459b32
arpita-acufore <arpita.s@acufore.com>
2023-06-27 10:52:41 +0530

    adding the files to check the pull request

--- test1.php
+++ test1.php
@@ -1,3 +1,14 @@
-<?php
-echo "this is the testing for test branch.";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "this is the testing for pull request merge in github.."
+    ?>
+    
+</body>
+</html>
